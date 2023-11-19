ced0516158cd6b2093770e35a2fdcdd8dde0fdcc
Klausenberg <tofangeorge23@gmail.com>
2023-11-19 17:22:23 +0200

    add penis len

--- budatest/src/App.js
+++ budatest/src/App.js
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
   render() {
     return (
       <div className="App">
@@ -11,7 +12,7 @@ class App extends Component {
           <h2>Welcome to React</h2>
         </div>
         <h1>
-          PENIS
+          PENIS B==D
         </h1>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
