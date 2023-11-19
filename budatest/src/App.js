1ba0cdb3d61d90baab8fde048304d617affa8c8c
Klausenberg <tofangeorge23@gmail.com>
2023-11-19 17:42:40 +0200

    add penis size limit

--- budatest/src/App.js
+++ budatest/src/App.js
@@ -4,6 +4,8 @@ import './App.css';
 
 function penisLen(len){
   let s ='';
+  if(len>20)
+    len = 20;
   for(let i=0; i<len; i++)
     s+='=';
   return s;
@@ -21,8 +23,9 @@ class App extends Component {
           <h2>Welcome to React</h2>
         </div>
         <h1>
-          PENIS B{penisLen(7)}D
+          PENIS B{penisLen(20)}D
         </h1>
+
         <p className="App-intro">
           To get fuck started, edit <code>src/App.js</code> and save to reload.
         </p>
