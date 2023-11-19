c32f9e9868fef83fcd1692abadccec4b7747698f
Klausenberg <tofangeorge23@gmail.com>
2023-11-19 17:12:14 +0200

    Added peinis header

--- budatest/src/App.js
+++ budatest/src/App.js
@@ -10,6 +10,9 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
+        <h1>
+          PENIS
+        </h1>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
